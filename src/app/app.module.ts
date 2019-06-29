519ffc718e83b7ba7de9a7d9ec99042ca6015754
Harry <hbishop11@gmail.com>
2019-06-29 14:43:52 +0200

    Improved folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ArticleSummaryComponent } from './article-summary/article-summary.component';
-import { ArticleBodyComponent } from './article-body/article-body.component';
-import { TimelineComponent } from './timeline/timeline.component';
-import { MenuComponent } from './menu/menu.component';
-import { TagsSelectorComponent } from './tags-selector/tags-selector.component';
-import { TagsCreatorComponent } from './tags-creator/tags-creator.component';
-import { SavedCheckboxComponent } from './saved-checkbox/saved-checkbox.component';
+import { ArticleSummaryComponent } from './components/article-summary/article-summary.component';
+import { ArticleBodyComponent } from './components/article-body/article-body.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TagsSelectorComponent } from './components/tags-selector/tags-selector.component';
+import { TagsCreatorComponent } from './components/tags-creator/tags-creator.component';
+import { SavedCheckboxComponent } from './components/saved-checkbox/saved-checkbox.component';
 
 @NgModule({
   declarations: [
